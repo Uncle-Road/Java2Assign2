2558a38f27a1e1396b62ca0dcf841db61a471f83
Uncle-Road <457894974@qq.com>
2022-11-02 22:14:18 +0800

    test

---
+++ src/application/test.java
@@ -0,0 +1,7 @@
+package application;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(com.sun.javafx.runtime.VersionInfo.getRuntimeVersion());
+    }
+}
